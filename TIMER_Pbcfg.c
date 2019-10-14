9839f3ed6300539e78587512cf93aff02c0ecc59
Mohammed Nassar <mohammedalaanassar@hotmail.com>
2019-10-14 21:55:08 +0200

    TIMER driver Source file Header file Configration Header file Post build header file Post build source file

---
+++ TIMER_Pbcfg.c
@@ -0,0 +1,16 @@
+/*
+ * TIMER_Pbcfg.c
+ *
+ *  Created on: Oct 13, 2019
+ *      Author: Mohammed
+ */
+
+
+#include "TIMER_Pbcfg.h"
+
+
+
+//TIMER_cnfg_t TIMER = {TIMER_ID,WGM_Mode,COM_mode,Prescaler,interrupt,INITIALIZATION};
+
+TIMER_cnfg_t TIMER = {TIMER0,NORMAL_MODE,NORMAL,PRESCALER0,INTERRUPT,NOT_INITIALIZED};
+
